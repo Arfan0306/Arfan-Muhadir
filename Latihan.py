216df79e89a5d3bf94caa7ba2a2e96804b434878
Arfan0306 <brarfan83@gmail.com>
2022-10-17 14:16:34 +0800

    day 1

--- Latihan.py
+++ Latihan.py
@@ -1,3 +1,28 @@
-nama = "arfan muhadir"
+lampu=input("masukan lampu merah : ")
 
-print(nama)
+if lampu == "merah":
+    print("berhenti")
+elif lampu == "kuning":
+    print("hati-hati")
+elif lampu == "hijau":
+    print("lanjut jalan")
+else:
+    print ("tidak ada dalam daftar")
+
+nilai = int(input("masukan nilai :"))
+if nilai >= 80:
+    print("A")
+elif nilai >= 60:
+    print("B")
+elif nilai >= 40:
+    print("C")
+else:
+    print("E")
+
+nilai1 = int(input("masukan nilai1 :"))
+nilai2 = int(input("masukan nilai2 :"))
+
+if nilai1:
+    a = nilai1 * nilai2
+    print (a)
+            
---
+++ days 1.py
@@ -0,0 +1,11 @@
+nama = "ARFAN MUHADIR"
+nim = "D0221530"
+prodi = "INFORMATIKA"
+kls = "INFORMATIKA B"
+alamat = "SENDANA"
+
+print (nama)
+print (nim)
+print (prodi)
+print (kls)
+print (alamat)
